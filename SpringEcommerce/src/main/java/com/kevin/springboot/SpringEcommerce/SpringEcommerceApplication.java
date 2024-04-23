5df14eb06a510c0af713419f0d08b96f4d575057
Kev <93066593+Dr-Hackerstein@users.noreply.github.com>
2024-04-23 13:09:34 -0600

    created the packages for the project

--- SpringEcommerce/src/main/java/com/kevin/springboot/SpringEcommerce/SpringEcommerceApplication.java
+++ SpringEcommerce/src/main/java/com/kevin/springboot/SpringEcommerce/SpringEcommerceApplication.java
@@ -2,8 +2,11 @@ package com.kevin.springboot.SpringEcommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+import javax.sql.DataSource;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class SpringEcommerceApplication {
 
 	public static void main(String[] args) {
