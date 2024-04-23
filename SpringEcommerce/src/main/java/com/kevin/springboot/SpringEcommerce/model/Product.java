33aef717d1ed79ca4358cec62c115784c41962c9
Kev <93066593+Dr-Hackerstein@users.noreply.github.com>
2024-04-23 13:33:05 -0600

    Created Product class

---
+++ SpringEcommerce/src/main/java/com/kevin/springboot/SpringEcommerce/model/Product.java
@@ -0,0 +1,11 @@
+package com.kevin.springboot.SpringEcommerce.model;
+
+public class Product {
+
+    private Integer id;
+    private String name;
+    private String description;
+    private String image;
+    private double price;
+    private int quantity;
+}
