4253fd796e937726899bbcffaabce0549d428db6
Kev <93066593+Dr-Hackerstein@users.noreply.github.com>
2024-04-23 13:33:57 -0600

    Created Product class and added getters and setters and contructors

--- SpringEcommerce/src/main/java/com/kevin/springboot/SpringEcommerce/model/Product.java
+++ SpringEcommerce/src/main/java/com/kevin/springboot/SpringEcommerce/model/Product.java
@@ -8,4 +8,64 @@ public class Product {
     private String image;
     private double price;
     private int quantity;
+
+    public Product() {
+    }
+
+    public Product(Integer id, String name, String description, String image, double price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.image = image;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
