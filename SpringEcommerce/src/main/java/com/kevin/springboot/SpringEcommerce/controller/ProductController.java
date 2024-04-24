cc4330d902ac98624d2fe04a8b0312b5b84ae2d5
Kev <93066593+Dr-Hackerstein@users.noreply.github.com>
2024-04-24 11:47:36 -0600

    Save Method Product Controller
    Created the save method in the product controller.

--- SpringEcommerce/src/main/java/com/kevin/springboot/SpringEcommerce/controller/ProductController.java
+++ SpringEcommerce/src/main/java/com/kevin/springboot/SpringEcommerce/controller/ProductController.java
@@ -1,13 +1,18 @@
 package com.kevin.springboot.SpringEcommerce.controller;
 
+import org.slf4j.*;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kevin.springboot.SpringEcommerce.model.Product;
 @Controller
 @RequestMapping("/products")
 public class ProductController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
     @GetMapping("")
     public String show() {
         return "products/show";
@@ -17,4 +22,12 @@ public class ProductController {
     public String create() {
         return "products/create";
     }
+
+    @PostMapping("/save")
+    public String save(Product product) {
+        LOGGER.info("Este es el objeto producto {}",product);
+        return "redirect:/products";
+    }
+
+
 }
