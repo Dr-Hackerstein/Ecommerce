f167f8221f10bb5cd8764284db6ec2de19010260
Kev <93066593+Dr-Hackerstein@users.noreply.github.com>
2024-04-25 10:57:02 -0600

    Added the save logic to the save method in Controller
    Implemented the save logic inside the save method inside the Product Controller

--- SpringEcommerce/src/main/java/com/kevin/springboot/SpringEcommerce/controller/ProductController.java
+++ SpringEcommerce/src/main/java/com/kevin/springboot/SpringEcommerce/controller/ProductController.java
@@ -2,18 +2,18 @@ package com.kevin.springboot.SpringEcommerce.controller;
 
 import com.kevin.springboot.SpringEcommerce.model.User;
 import com.kevin.springboot.SpringEcommerce.service.ProductService;
+import com.kevin.springboot.SpringEcommerce.service.UploadFileService;
 import org.slf4j.*;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import com.kevin.springboot.SpringEcommerce.model.Product;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Optional;
 import java.util.PrimitiveIterator;
 
@@ -24,6 +24,8 @@ public class ProductController {
     private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
     @Autowired
     private ProductService productService;
+    @Autowired
+    private UploadFileService upload;
     @GetMapping("")
     public String show(Model model) {
         model.addAttribute("products",productService.findAll());
@@ -36,10 +38,26 @@ public class ProductController {
     }
 
     @PostMapping("/save")
-    public String save(Product product) {
+    public String save(Product product, @RequestParam("img") MultipartFile file) throws IOException {
         LOGGER.info("Este es el objeto producto {}",product);
         User user = new User(1,"","","","","","","");
         product.setUser(user);
+
+        //image
+        if(product.getId()==null){ // when a product is created
+            String imageName = upload.saveImage(file);
+            product.setImage(imageName);
+        }else {
+            if(file.isEmpty()){ //edit the product but not change the image
+                Product p = new Product();
+                p = productService.get(product.getId()).get();
+                product.setImage(p.getImage());
+            }else {
+                String imageName = upload.saveImage(file);
+                product.setImage(imageName);
+            }
+        }
+
         productService.save(product);
         return "redirect:/products";
     }
