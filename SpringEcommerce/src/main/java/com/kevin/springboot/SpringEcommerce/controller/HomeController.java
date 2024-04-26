42c15614a035f2bfcfd5cd6148778321ce8ddbe8
Kev <93066593+Dr-Hackerstein@users.noreply.github.com>
2024-04-26 10:13:17 -0600

    Created producthome.html and add the methods in the Home Controller

--- SpringEcommerce/src/main/java/com/kevin/springboot/SpringEcommerce/controller/HomeController.java
+++ SpringEcommerce/src/main/java/com/kevin/springboot/SpringEcommerce/controller/HomeController.java
@@ -1,15 +1,23 @@
 package com.kevin.springboot.SpringEcommerce.controller;
 
+import com.kevin.springboot.SpringEcommerce.model.Product;
 import com.kevin.springboot.SpringEcommerce.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+    private final Logger log = LoggerFactory.getLogger(HomeController.class);
     @Autowired
     private ProductService productService;
     @GetMapping("")
@@ -17,5 +25,17 @@ public class HomeController {
 
         model.addAttribute("products",productService.findAll());
         return "users/home";
+    }
+
+    @GetMapping("producthome/{id}")
+    public String productHome(@PathVariable Integer id, Model model) {
+        log.info("Id producto enviado como parámetro {}", id);
+        Product product = new Product();
+        Optional<Product> productOptional = productService.get(id);
+        product = productOptional.get();
+
+        model.addAttribute("product", product);
+
+        return "users/producthome";
     }
 }
