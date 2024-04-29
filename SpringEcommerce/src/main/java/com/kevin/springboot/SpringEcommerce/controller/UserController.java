12c2b867654f81bcaa56ebba9c34159d46b463b0
Kev <93066593+Dr-Hackerstein@users.noreply.github.com>
2024-04-29 11:13:53 -0600

    Created the UserController and added the Register Method

--- SpringEcommerce/src/main/java/com/kevin/springboot/SpringEcommerce/controller/UserController.java
+++ SpringEcommerce/src/main/java/com/kevin/springboot/SpringEcommerce/controller/UserController.java
@@ -1,2 +1,33 @@
-package com.kevin.springboot.SpringEcommerce.controller;public class UserController {
+package com.kevin.springboot.SpringEcommerce.controller;
+
+import com.kevin.springboot.SpringEcommerce.service.IOrderService;
+import com.kevin.springboot.SpringEcommerce.service.IUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    private final Logger logger= LoggerFactory.getLogger(UserController.class);
+
+    @Autowired
+    private IUserService userService;
+
+    @Autowired
+    private IOrderService orderService;
+
+    // BCryptPasswordEncoder passEncode= new BCryptPasswordEncoder();
+
+
+    // /usuario/registro
+    @GetMapping("/register")
+    public String create() {
+        return "users/register";
+    }
+
 }
